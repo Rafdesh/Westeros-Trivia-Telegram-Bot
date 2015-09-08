5263689a21a496a0ab4e30b2d694a75cf555c95b
EklavyaFCB <eklavya1994@yahoo.co.in>
2015-09-08 22:48:46 +0200

    Updates, now can send messages and receive updates using twx

--- PythonBot/Bot.py
+++ PythonBot/Bot.py
@@ -3,27 +3,43 @@
 import string
 import json
 import random
+import sys
+import requests
 from pprint import pprint
+from twx.botapi import TelegramBot, ReplyKeyboardMarkup
+
+def get_me(**kwargs):
+    """
+    A simple method for testing your bot's auth token. Requires no parameters.
+    Returns basic information about the bot in form of a User object.
+    :param \*\*kwargs: Args that get passed down to :class:`TelegramBotRPCRequest`
+    :returns: Returns basic information about the bot in form of a User object.
+    :rtype: User
+    """
+    return TelegramBotRPCRequest('getMe', on_result=User.from_result, **kwargs)
 
 
 # Token Constants
 
-BASE_URL = 'https://api.telegram.org/bot'
-TOKEN = '125579945:AAGjgsLLGeSqSILvfky71QjuMcZpeIBpuho/'
-MAIN_URL = 'cURL -s -X POST' + BASE_URL + TOKEN + '-d text="A message from your bot"' + '-d chat_id="33843334"'
-
-
-def sendMessage():
-	print(MAIN_URL)
-	# curl -s -X POST https://api.telegram.org/bot125579945:AAGjgsLLGeSqSILvfky71QjuMcZpeIBpuho/sendMessage \ -d text="A message from your bot" \ -d chat_id=33843334
-	#print('curl -s -X POST https://api.telegram.org/bot125579945:AAGjgsLLGeSqSILvfky71QjuMcZpeIBpuho/sendMessage -d text="A message from your bot" -d chat_id=33843334')
+bot = TelegramBot('125579945:AAGjgsLLGeSqSILvfky71QjuMcZpeIBpuho')
+#bot.update_bot_info().wait()
+#print(bot.username)
 
 
 json_file = 'data.json'
 json_data = open(json_file)
 data = json.load(json_data)
+json_data.close()
 
 #print data['result'][0]['message']['text']
+
+# Renaming the JSON objects as string variables so that I don't have to
+# type "data['x'][0]['y']['z']" all the time:
+
+
+#text = data['result'][0]['message']['text']
+#message_id = data['result'][0]['message']['message_id']
+#chat_id = data['result'][0]['message']['chat']['id']
 
 
 QuestionsList = [
@@ -32,30 +48,80 @@ QuestionsList = [
 'Is Jon Snow really dead?',
 'Who killed Ygritte?',
 'Why does Arya go blind?',
-'Who is the prettiest girl in the show?'
+'Who is the prettiest girl in the show?',
+'What promise did Lyanna force Ned to make before her death?',
+'Who thought himself to be the prince who was promised?',
+'Which prominent noble family currently seems to have a lot of wargs?',
+'Where do the dragon eggs originate from?',
+'Who is the man Bran meets at the end of the season 4, seemingly living under a weirwood tree?',
+'What do the weirwood trees represent?',
+'Who orchestrated the murder of Jon Arryn?',
+'Who killed Joffrey?',
+'Why did Jaime tell Tyrion that caused him to kill Tywin?',
+'What species were the first humanoid creatures to walk on Westeros?',
+'Which famous man bought down almost all Westeros to heel?'
 ]
 
-arrayEnd = len(QuestionsList)
+arrayEnd = len(QuestionsList) - 1
 
 i = random.randint(0, arrayEnd)
 
 
-if data['result'][0]['message']['text'] == '/question':
-	#print QuestionsList[i]
-	sendMessage()
+user_id = int(33843334)
+last_update_id = 0 #last processed command
 
 
-'''if text.contains("/question"):
-	reply = "Is Cersei dead?"
-	sendMessage(chat_id,reply)
-else
-	reply = "False"'''
-
-json_data.close()
+bot.send_message(user_id, 'Hello, please use the /question command to start the trivia!').wait()
 
 
+'''while True:
+    updates = bot.get_updates(offset = last_update_id + 1, limit = None, timeout=None).wait()
+    
+
+    r = requests.get('https://api.telegram.org/bot125579945:AAGjgsLLGeSqSILvfky71QjuMcZpeIBpuho/getUpdates')
+    if r.status_code == 200:
+
+        if not updates:
+            continue
+
+        else:
+            #len(responses) - 1
+            update_id = updates[0][0]
+            last_update_id = update_id + 1
+
+        for i in range(len(updates)): # probably range(len(updates[0][0]))
+
+            # Process commands: 
+            message = updates[i][1]
+            chat_id = updates[i][1][3][0]
+            username = updates[i][1][1][3]
+            text = updates[i][1][7]
+
+            print text
+
+            i = i + 1
 
 
-# curl -s -X POST https://api.telegram.org/bot125579945:AAGjgsLLGeSqSILvfky71QjuMcZpeIBpuho/sendMessage \ -d text="A message from your bot" \ -d chat_id=33843334
+            if '/question' in text:
+                bot.send_message(user_id, QuestionsList[i]).wait()'''
+                
 
+
+updates = bot.get_updates(offset=None,limit=2,timeout=None).wait()
+
+for update in updates:
+    gaga = updates[0][0]
+    print(gaga)
+
+
+##############################################################################
+
+# ACCORDING TO BOT ID:
+
+# Eklavya = 33843334
+# Bot-en-stein = -39649874
+# 2 idiots = -20170726
+# Los Barbokerinos = -7635063
+# Liridon = 48389492
+# Karan = 61770511
 
